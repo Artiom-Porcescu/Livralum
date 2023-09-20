583ff338cdddc220b52667b0660d3aea1be9ab21
Artiom Porcescu <porchiesku@bk.ru>
2023-09-20 11:17:38 +0300

    Initial Commit

---
+++ Livralum/LivralumApp.swift
@@ -0,0 +1,17 @@
+//
+//  LivralumApp.swift
+//  Livralum
+//
+//  Created by Artiom Porcescu on 20.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct LivralumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
